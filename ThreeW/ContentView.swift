7191fe338ea51c0ab9f38bef664e12eeb17cc0aa
Kael <Kael.zs17@gmail.com>
2022-05-17 19:44:57 +0800

    Initial Commit

---
+++ ThreeW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThreeW
+//
+//  Created by Kael on 2022/5/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThreeW/ThreeWApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreeWApp.swift
+//  ThreeW
+//
+//  Created by Kael on 2022/5/17.
+//
+
+import SwiftUI
+
+@main
+struct ThreeWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
